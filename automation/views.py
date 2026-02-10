9e34975f1b842e58d370255f2ef83a6525edb456
Victor <victorfariasbezerra12@gmail.com>
2026-02-10 14:58:02 -0300

    About Page

--- automation/views.py
+++ automation/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'automation/home.html')
+
+def about(request):
+    return render(request, 'automation/about.html')
