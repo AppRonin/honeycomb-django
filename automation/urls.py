dd6fb8568c4c943d3537e87cf25a94cb1f76fca5
Victor <victorfariasbezerra12@gmail.com>
2026-02-19 00:54:03 -0300

    Fixing URL

--- automation/urls.py
+++ automation/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('gpon-conversor', views.gpon_conversor, name='gpon_conversor'),
+    path('about/', views.about, name='about'),
+    path('gpon-conversor/', views.gpon_conversor, name='gpon_conversor'),
     path("progress/<task_id>/", views.task_progress, name="task-progress"),
 ]
